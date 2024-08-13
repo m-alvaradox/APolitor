cd6a112f4154af5aef29f1bff4aa51c242013ad9
YadiSuarez <ymsuarez@espol.edu.ec>
2024-08-12 21:37:03 -0500

    implementacion goJuego
    se muestra las preguntas, e interaccion con el juego

--- Proy2P_ED/src/main/java/TDAs/DecisionTree.java
+++ Proy2P_ED/src/main/java/TDAs/DecisionTree.java
@@ -1,7 +1,11 @@
 
 package TDAs;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
 public class DecisionTree<E> {
     
@@ -61,5 +65,49 @@ public class DecisionTree<E> {
         } else {
             root.getNoBranch().recorrerArbol();
         }
-    }   
+    } 
+    
+    public DecisionTree<E> findParent(NodeDecisionTree<E> nodo){
+       DecisionTree<E> parent=null;
+       Stack<DecisionTree<E>> copia = new Stack<>();
+       if(!this.isEmpty() &&  nodo != null){
+           copia.push(this);
+           while(!copia.isEmpty()){
+               DecisionTree<E> tree = copia.pop();
+               boolean yesBranch = tree.getRoot().getYesBranch()!=null;
+               boolean noBranch = tree.getRoot().getNoBranch()!=null;
+               if( yesBranch && tree.getRoot().getYesBranch().root.getContent().equals(nodo.getContent()) || noBranch && tree.getRoot().getNoBranch().root.getContent().equals(nodo.getContent())){
+                   parent = tree;
+                   return parent;
+               }
+               if(yesBranch){
+                   copia.push(tree.getRoot().getYesBranch());
+               }
+               if(noBranch){
+                   copia.push(tree.getRoot().getNoBranch());
+               }
+           }
+       }    
+       return parent; 
+    } 
+    
+    public ArrayList<DecisionTree> obtenerLeaves() {
+        ArrayList<DecisionTree> hojas = new ArrayList<>();
+
+        // Si el nodo actual es una hoja (no tiene hijos)
+        if (this.isLeaf()) {
+            hojas.add(this);
+        } else {
+            // Si tiene un hijo izquierdo, busca recursivamente
+            if (this.root.getYesBranch() != null) {
+                hojas.addAll(this.root.getYesBranch().obtenerLeaves());
+            }
+            // Si tiene un hijo derecho, busca recursivamente
+            if (this.root.getNoBranch() != null) {
+                hojas.addAll(this.root.getNoBranch().obtenerLeaves());
+            }
+        }
+
+        return hojas;
+    }
 }
--- Proy2P_ED/src/main/java/com/espol/proy2p_ed/GoJuegoController.java
+++ Proy2P_ED/src/main/java/com/espol/proy2p_ed/GoJuegoController.java
@@ -5,12 +5,23 @@
 package com.espol.proy2p_ed;
 
 import Objects.Juego;
+import TDAs.DecisionTree;
+import TDAs.NodeDecisionTree;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;  
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 
 /**
@@ -35,8 +46,31 @@ public class GoJuegoController implements Initializable {
     @FXML
     ImageView imagenLogo;
     
+    @FXML
+    ImageView tortuga;
+    @FXML
+    VBox conteinerButton;
     Juego JuegoAUsar;
-    
+    DecisionTree<String> ArbolJuego;
+    int levelQuestions;
+    int numQuestionsUsar;
+    int contador=0; // contador que sirve para no acceder el limite de preguntas que eligió el jugador
+    @FXML
+    AnchorPane containerDetalles;
+    @FXML
+    TextField numQuestions;
+    @FXML
+    Label Question;
+    @FXML
+    ToggleGroup botonDecision;
+    @FXML
+    Button botonSi;
+    @FXML
+    Button botonNo;
+    @FXML
+    Button botonCorregir;
+    @FXML
+    Button botonReiniciar;
     /**
      * Initializes the controller class.
      */
@@ -47,10 +81,151 @@ public class GoJuegoController implements Initializable {
         t1.Transitionround(c1,c2,c3,c4,c5,c6);
         t1.rotateLogo(imagenLogo);
         JuegoAUsar= JuegosController.JuegoEnUso;
+        try {
+            ArbolJuego = App.buildDecisionTree(JuegoAUsar.getPreguntas(), JuegoAUsar.getAnswers());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        levelQuestions = ArbolJuego.getElements().size();
+        
     }    
+    
+    private void actualizarVentana(){
+        contador++;
+        if(contador==1){
+            botonCorregir.setVisible(false);
+        }
+        if(contador<=numQuestionsUsar){
+            Question.setText(contador+". "+ArbolJuego.getRoot().getContent());               
+        }else{
+            mostrarPosiblesResultados();
+        }
+       
+    }
+    
+    private void mostrarResultado(){
+        botonReiniciar.setVisible(true);
+        botonNo.setVisible(false);
+        botonSi.setVisible(false);
+        botonCorregir.setVisible(false);
+        Question.setText("Lo que pensó fue un "+ ArbolJuego.getRoot().getContent());
+    }
+    
+    private void mostrarNoEncontrado(){
+        botonReiniciar.setVisible(true);
+        botonNo.setVisible(false);
+        botonSi.setVisible(false);
+        botonCorregir.setVisible(false);
+        tortuga.setImage(new Image("/imagenes/tortugaTriste.png"));
+        Question.setText("Que pena, no pude adivinar :( ");
+    }
+    
+    private void mostrarPosiblesResultados(){
+        botonReiniciar.setVisible(true);
+        conteinerButton.getChildren().removeAll(botonNo, botonSi, botonCorregir);
+        Question.setText("Con sus números de preguntas se obtuvo estas posibles respuestas");
+        ArrayList<DecisionTree> hojas = ArbolJuego.obtenerLeaves();
+        for(DecisionTree hoja:hojas){
+            Label lb = new Label();
+            lb.setText(hoja.getRoot().getContent().toString());
+            lb.getStyleClass().add("text-atributos");
+            conteinerButton.getChildren().add(lb);
+        }
+        
+    }
+    
+    @FXML
+    private void Reiniciar() throws IOException{
+        App.setRoot("goJuego");
+    }
+    
+    @FXML
+    private void botonSi() throws IOException {
+        if(contador==1){
+            botonCorregir.setVisible(true);
+        }
+        if (ArbolJuego.getRoot().getYesBranch()!=null) {
+            ArbolJuego = ArbolJuego.getRoot().getYesBranch();
+            if (ArbolJuego.isLeaf()) {
+                mostrarResultado();
+            } else {
+                actualizarVentana();
+            }
+        } else {
+            mostrarNoEncontrado();
+        }  
+    }
+
+    @FXML
+    private void botonNo() throws IOException {
+        if(contador==1){
+            botonCorregir.setVisible(true);
+        }
+        if (ArbolJuego.getRoot().getNoBranch()!=null) {
+            ArbolJuego = ArbolJuego.getRoot().getNoBranch();
+            if (ArbolJuego.isLeaf()) {
+                mostrarResultado();
+            } else {
+                actualizarVentana();
+            }
+        } else {
+            mostrarNoEncontrado();
+        } 
+    }
+    
+    @FXML
+    private void questionAnterior(){
+        DecisionTree<String> ArbolOriginal = null;
+        try {
+            ArbolOriginal = App.buildDecisionTree(JuegoAUsar.getPreguntas(), JuegoAUsar.getAnswers());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        DecisionTree<String> padre = ArbolOriginal.findParent(ArbolJuego.getRoot());
+        ArbolJuego = padre;
+        contador-=2;
+        actualizarVentana();
+    }
     
     @FXML
     private void regresar() throws IOException {
         App.setRoot("juegos");
+    }
+    
+    @FXML
+    private void OmitirQuestion() throws IOException{
+        containerDetalles.setVisible(false);
+        numQuestionsUsar = levelQuestions;
+        actualizarVentana();    
+    }
+    
+    @FXML
+    private void EmpezarJuego() throws IOException{
+        if(numQuestions.getText()!=null && !numQuestions.getText().isEmpty()){
+           int levels = Integer.parseInt(numQuestions.getText());
+            if(levels > levelQuestions){
+                mostraralertaerror("Este numero excede el numero de preguntas");
+            } else {
+                numQuestionsUsar = levels;
+                containerDetalles.setVisible(false);
+                actualizarVentana();
+            } 
+        }else{
+            mostraralertaerror("Digite un numero");
+        }
+            
+    }
+    
+    public void mostraralertaerror(String msg) {
+        
+        Alert alert= new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText("Operación Cancelada");
+        alert.setTitle("Error");
+        String msgerr = String.format(msg);
+        alert.setContentText(msgerr);
+        String css = this.getClass().getResource("/styles/estilos.css").toExternalForm();
+        alert.getDialogPane().getStylesheets().add(css);
+        alert.getDialogPane().getStyleClass().add("dialog-paneError");
+        alert.showAndWait();      
     }
 }
